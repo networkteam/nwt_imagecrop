09b5f64a3473eb6ce882993865b8debed018b57d
Christoph Lehmann <lehmann@networkteam.com>
2016-11-25 11:28:02 +0100

    [TASK] Refactor CSH

--- ext_tables.php
+++ ext_tables.php
@@ -4,7 +4,7 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_content', 'EXT:nwt_imagecrop/locallang_csh_tt_content.php');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_content', 'EXT:nwt_imagecrop/Resources/Private/Language/locallang_csh_tt_content.xlf');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Image Cropscaling');
 ?>
